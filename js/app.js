cb7bc79020da73857e861d79cf9a66f6bf0db596
ahmed-shiiqaaye <me.shiqaye@gmail.com>
2022-05-21 00:28:45 +0200

    Home pag is almost done

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ let prevtBtn = document.querySelectorAll('.prev');
 
 sliderContainer.forEach((slider,i) =>{
     nextBtn[i].addEventListener('click',()=>{
-        slider.scrollLeft += 620 
+        slider.scrollLeft += slider.getBoundingClientRect().width 
     })
     prevtBtn[i].addEventListener('click',()=>{
-        slider.scrollLeft -= 620
+        slider.scrollLeft -= slider.getBoundingClientRect().width 
     })
 })
 
